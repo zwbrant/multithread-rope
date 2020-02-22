a8768a069039acd8e33f2bb650d2bf81c5572fdb
zwbrant <zwbrant@uw.edu>
2020-02-22 07:31:00 -0800

    Init

---
+++ Assets/thingy1.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Jobs;
+using Unity.Burst;
+using Unity.Collections;
+using static Unity.Mathematics.math;
+
+public class thingy1 : MonoBehaviour
+{
+    public List<GameObject> Cubes;
+
+    NativeArray<float> CubeHeights;
+    NativeList<float> salmon;
+
+    JobHandle longHandle;
+
+    private void Awake()
+    {
+        
+        //print("Awake1");
+        CubeHeights = new NativeArray<float>(5, Allocator.Persistent);
+        //Debug.Log("Before: " + System.DateTime.Now.Second);
+
+        //var longJob = new LongJob();
+        //longHandle = longJob.Schedule(14, 1);
+
+
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Start1");
+
+    }
+
+    JobHandle cubeJHandle;
+    // Update is called once per frame
+    void Update()
+    {
+        var cubeJob = new CubeJob() { heights = CubeHeights, time = Time.time };
+        cubeJHandle = cubeJob.Schedule(5, 1);
+
+
+
+        //print(sin(3.14159265359f / 2));
+    }
+
+    private void LateUpdate()
+    {
+        cubeJHandle.Complete();
+        UpdateCubePositions();
+        //Debug.Log("After: " + System.DateTime.Now.Second);
+
+    }
+
+    private void UpdateCubePositions()
+    {
+        for (int i = 0; i < Cubes.Count; i++)
+        {
+            Cubes[i].transform.position = new Vector3(Cubes[i].transform.position.x, CubeHeights[i]);
+        }
+    }
+
+    [BurstCompile(CompileSynchronously = true)]
+    public struct CubeJob : IJobParallelFor
+    {
+        public float time;
+        public NativeArray<float> heights;
+
+        public void Execute(int index)
+        {
+            heights[index] = sin(time + index);
+        }
+    }
+
+    public struct LongJob : IJobParallelFor
+    {
+        public void Execute(int i)
+        {
+
+                int count = 0;
+                long a = 2;
+                while (count < 400000)
+                {
+                    long b = 2;
+                    int prime = 1;// to check if found a prime
+                    while (b * b <= a)
+                    {
+                        if (a % b == 0)
+                        {
+                            prime = 0;
+                            break;
+                        }
+                        b++;
+                    }
+                    if (prime > 0)
+                    {
+                        count++;
+                    }
+                    a++;
+                }
+                
+            
+        }
+    }
+}
+
---
+++ Assets/thingy2.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Jobs;
+using Unity.Burst;
+using Unity.Collections;
+using static Unity.Mathematics.math;
+using Unity.Mathematics;
+
+public class thingy2 : MonoBehaviour
+{
+    public float Gravity = -1f;
+    public List<GameObject> PointGOs;
+    public NativeList<Point> Points;
+
+
+
+    void Start()
+    {
+        // print("Start2");
+        Points = new NativeList<Point>(Allocator.Persistent);
+        Points.Add(new Point() { OldPos = new float3(0, 5, 0), Pos = new float3(0, 5, 0) });
+
+    }
+
+    void Update()
+    {
+
+        var job = new VerletJob() {
+            DeltaTime = Time.deltaTime,
+            Points = Points,
+            Gravity = Gravity
+        };
+
+        var handle = job.Schedule(Points.Length, 1);
+
+        handle.Complete();
+
+        // transform GOs based on new points
+        for (int i = 0; i < PointGOs.Count; i++)
+        {
+            if (Points.Length <= i)
+                break;
+
+            PointGOs[i].transform.position = Points[i].Pos;
+        }
+
+
+    }
+}
+
+
+// all the data needed to represent a Verlet point/particle 
+public struct Point
+{
+    public float3 Pos;
+    public float3 OldPos;
+}
+
+public struct VerletJob : IJobParallelFor
+{
+    public NativeArray<Point> Points;
+    public float Gravity;
+    public float DeltaTime;
+
+    public void Execute(int i)
+    {
+        UpdateVerlet(i);
+    }
+
+    public void UpdateVerlet(int i)
+    {
+        var point = Points[i];
+      
+        float3 velocity = point.Pos - point.OldPos;
+        velocity.y += Gravity;
+
+        point.OldPos = point.Pos;
+        point.Pos = point.Pos + velocity;
+
+        Points[i] = point;
+    }
+}
